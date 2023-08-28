511e240b451d06a9ae3c7912f6defe92badc94ad
Nguyen Minh Tuan <tnowad@gmail.com>
2023-08-29 02:57:31 +0700

    feat(auth): add auth with next-auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+import { NextApiHandler } from "next";
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GitHubProvider from "next-auth/providers/github";
+import prisma from "@/lib/prisma";
+
+const handler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+
+export { handler as GET, handler as POST };
+
+const options = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+};

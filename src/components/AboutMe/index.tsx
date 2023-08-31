daf699784b3f634195dd013ad6eab0e2147ddb3d
Nguyen Minh Tuan <tnowad@gmail.com>
2023-08-31 08:19:01 +0700

    feat(home): add animation for about me

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { useEffect } from "react";
 import Image from "next/image";
 import AvatarImage from "@/assets/avatar.png";
@@ -17,18 +18,32 @@ const AboutMe = () => {
     if (inView) {
       controls.start({ opacity: 1, x: 0 });
     } else {
-      controls.start({ opacity: 0, x: -20 });
+      controls.start({ opacity: 0, x: -50 });
     }
   }, [inView, controls]);
 
+  const staggerChildren = {
+    animate: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const fadeInUp = {
+    initial: { opacity: 0, y: 20 },
+    animate: { opacity: 1, y: 0 },
+    transition: { duration: 0.5 },
+  };
+
   return (
-    <section className="flex justify-center">
+    <section className="my-8 flex justify-center" id="about-me">
       <div className="container flex grid-cols-12 flex-col items-center md:grid">
         <motion.div
           ref={ref}
-          initial={{ opacity: 0, x: -20 }}
+          initial={{ opacity: 0, x: -50 }}
           animate={controls}
-          transition={{ duration: 0.5 }}
+          transition={{ duration: 0.8 }}
           className="relative col-span-5 h-80 w-80 lg:col-span-4"
         >
           <div className="h-full w-full rounded-full bg-white" />
@@ -44,34 +59,35 @@ const AboutMe = () => {
             Nguyen Minh Tuan
           </h2>
           <motion.div
-            initial={{ opacity: 0, x: -20 }}
+            initial={{ opacity: 0, x: -50 }}
             animate={controls}
-            transition={{ duration: 0.5, delay: 0.2 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
             className="text-md"
+            variants={staggerChildren}
           >
-            <p>
+            <motion.p {...fadeInUp}>
               👋 Hey there, I&lsquo;m Nguyen Minh Tuan, and my software journey
               began in 2021!
-            </p>
-            <p>
+            </motion.p>
+            <motion.p {...fadeInUp}>
               🚀 I&lsquo;m all about the exciting Node.js stack, crafting with
               React.js, Express.js, and bringing together MongoDB, MySQL, and
               Docker.
-            </p>
-            <p>
+            </motion.p>
+            <motion.p {...fadeInUp}>
               🎓 Lifelong learner alert! I&lsquo;m passionate about bridging the
               gap between code and creativity, and I&lsquo;m always on the
               lookout for ways to improve.
-            </p>
-            <p>
+            </motion.p>
+            <motion.p {...fadeInUp}>
               🌈 Every day, I&lsquo;m on a mission to level up and exceed my
               past achievements. My goal? To be a little better than I was
               yesterday!
-            </p>
-            <p>
+            </motion.p>
+            <motion.p {...fadeInUp}>
               🌟 Let&lsquo;s embark on a journey of coding, learning, and
               creating together – the future is looking incredibly bright!
-            </p>
+            </motion.p>
           </motion.div>
         </div>
       </div>
